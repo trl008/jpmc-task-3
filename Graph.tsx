5bdba213dac7a5b2f9f08009a730196eb1525691
trl008 <94184418+trl008@users.noreply.github.com>
2022-09-28 15:37:00 -0400

    Add files via upload

---
+++ Graph.tsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Table } from '@jpmorganchase/perspective';
+import { ServerRespond } from './DataStreamer';
+import { DataManipulator } from './DataManipulator';
+import './Graph.css';
+
+interface IProps {
+  data: ServerRespond[],
+}
+
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void,
+}
+class Graph extends Component<IProps, {}> {
+  table: Table | undefined;
+
+  render() {
+    return React.createElement('perspective-viewer');
+  }
+
+  componentDidMount() {
+    // Get element from the DOM.
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    
+    // Update schema to track ratios of two stocks as oppsed to distinguishing between the two stocks. Track upper and lower bound 
+    // and when bounds are crossed with trigger_alert. All is being tracked with respect to time
+    const schema = {
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
+      timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
+    };
+
+    if (window.perspective && window.perspective.worker()) {
+      this.table = window.perspective.worker().table(schema);
+    }
+    if (this.table) {
+      // Load the `table` in the `<perspective-viewer>` DOM reference.
+      // Modify/add attributes to element. The graph type is y_line. 
+      // Datapoints are mapped based on timestamp on x-axis
+      // Track ratio, lower_bound, upper_bound, and trigger_alert on y-axis for each datapoint
+      // Handle cases of duplicated data by consolidating to just one data point. Datapoints only unique if it has timestamp
+      elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+      elem.setAttribute('aggregates', JSON.stringify({
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
+        timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
+      }));
+    }
+  }
+
+  // Update argument in this.table.update
+  componentDidUpdate() {
+    if (this.table) {
+      this.table.update([
+        DataManipulator.generateRow(this.props.data),
+      ]);
+    }
+  }
+}
+
+export default Graph;
